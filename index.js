8ed668073a12f35a71f53fc523f98b2a0d7f0256
seda615 <grigoryanseda96@gmail.com>
2021-05-10 13:30:08 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload=main;
+
+function main() {
+    const toggleButton = document.getElementsByClassName("toggle-button")[0];
+    const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+    toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+})
+}
